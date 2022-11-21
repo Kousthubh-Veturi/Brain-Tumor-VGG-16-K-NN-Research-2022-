90bedf4d0ef1c733b10edc35deea7d04021ebf7c
Kousthubh-Veturi <kousthubh.veturi@gmail.com>
2022-11-21 13:12:08 -0800

    -

--- projlib.py
+++ projlib.py
@@ -12,16 +12,22 @@ import tensorflow as tf
 import keras
 from keras import models 
 from keras.applications import vgg16
+from keras.applications import vgg19
 from keras.applications import resnet
 from keras.models import Model
+from keras.applications import efficientnet_v2
+from keras.applications import nasnet
 import numpy as np
 import sklearn
 from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import KNeighborsClassifier
+from keras.applications import inception_v3
 import pickle as pkl
 import glob
-
-
+from typing import Any
+import copy
+from copy import deepcopy
+import matplotlib.pyplot as plt
 def generate_features(model_name:str,layer_name:str):
     '''
     before calling function, define following vars:
@@ -31,26 +37,33 @@ def generate_features(model_name:str,layer_name:str):
     '''
     
     if model_name == "VGG16":
-        model = vgg16.VGG16()
+        model = vgg16.VGG16(include_top=False)
         prep = vgg16.preprocess_input
-         
+        kept_layers = [] 
+        for layer in model.layers:
+            kept_layers.append(layer)
+            if(layer.name==layer_name):
+                break
+        
+        model = keras.Sequential(kept_layers) 
     else:
-        model = resnet.ResNet50()
-        prep = resnet.preprocess_input
-
-    kept_layers = []
-    for layer in model.layers:
-        kept_layers.append(layer)
-        if(layer.name==layer_name):
-            break
-    model = keras.Sequential(kept_layers) 
+        model = vgg19.VGG19(include_top=False)
+        prep = vgg19.preprocess_input
+        kept_layers = []
+        for layer in model.layers:
+            kept_layers.append(layer)
+            if(layer.name==layer_name):
+                break
+        
+        model = keras.Sequential(kept_layers) 
+    
   
 
     for split_name in ["training","testing"]:
         source = Path(os.environ["MRI_DATA_PATH"],split_name)
         class_dirs = sorted(source.glob("[!.]*"))
         for class_dir in class_dirs:
-            for file_path in class_dir.iterdir():
+            for file_path in class_dir.glob("[!.]*"): 
                 #dest = Path("artifacts/vgg_features/",split_name,class_dir.name,file_path.stem).with_suffix(".npy") 
                 dest = Path("artifacts/",model_name,layer_name,"features",split_name,class_dir.name,file_path.stem).with_suffix(".npy")  
                 image = tf.keras.preprocessing.image.load_img(file_path,target_size=(224,224))
@@ -64,7 +77,7 @@ def generate_features(model_name:str,layer_name:str):
                 np.save(dest,features)
  
 
-def train_nearest_neighbor_classifier(model_name:str,layer_name:str): 
+def train_classifier(model_name:str,layer_name:str,classifier:Any): 
     features = []
     labels = []
     source = Path("artifacts/"+model_name+"/"+layer_name+"/features/training")
@@ -81,14 +94,19 @@ def train_nearest_neighbor_classifier(model_name:str,layer_name:str):
     
     features = np.array(features)
     labels = np.array(labels)
-    knc = KNeighborsClassifier(n_neighbors=1)
-    knc.fit(features,labels) 
-    dest = Path("artifacts/models/knc.pkl")
+
+
+
+    #knc = KNeighborsClassifier(n_neighbors=1)
+    classifier = deepcopy(classifier)
+    classifier.fit(features,labels) 
+    
+    dest = Path("artifacts/classifiers/",model_name,layer_name,str(classifier)).with_suffix(".pkl") 
     dest.parent.mkdir(parents=True,exist_ok=True)
-    dest.write_bytes(pkl.dumps(knc)) 
+    dest.write_bytes(pkl.dumps(classifier)) 
     print("saved model") 
 
-def test_nearest_neighbor_classifier(model_name:str,layer_name:str):
+def test_classifier(model_name:str,layer_name:str,classifier:Any):
     features = []
     labels = []
     source = Path("artifacts/"+model_name+"/"+layer_name+"/features/testing")
@@ -102,8 +120,11 @@ def test_nearest_neighbor_classifier(model_name:str,layer_name:str):
      
     labels = np.array(labels)
     features = np.array(features)
-    model = pkl.load(open("artifacts/models/knc.pkl", 'rb'))
+    location_of_classifier = Path("artifacts/classifiers/",model_name,layer_name,str(classifier)).with_suffix(".pkl") 
+    #model = pkl.load(open(location_of_classifier, 'rb')) 
+    model = pkl.loads(location_of_classifier.read_bytes())
     predictions = model.predict(features) 
+
 
     print(np.mean(labels==predictions)*100 , "%")
     pred_counts = np.zeros((4,4))
@@ -113,7 +134,15 @@ def test_nearest_neighbor_classifier(model_name:str,layer_name:str):
     print(pred_counts)
     pred_rates = (pred_counts/(np.sum(pred_counts,axis=1,keepdims=True)))
     print(pred_rates)
-    
+
+    figdest = Path("artifacts/figures/",model_name,layer_name,str(classifier),"confusion_matrix.pdf")
+    figdest.parent.mkdir(parents=True,exist_ok=True)
+    plt.figure(figsize=(3,3))
+    plt.matshow(pred_rates,cmap="RdYlGn")
+    plt.savefig(figdest)
+    plt.close()
+
+
         
 
 
